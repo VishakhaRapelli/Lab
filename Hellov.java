cbac23edbf3b6f886e047142f5e41228b57da158
VishakhaRapelli <125191036+VishakhaRapelli@users.noreply.github.com>
2023-03-16 14:29:17 +0530

    Add files via upload

---
+++ Hellov.java
@@ -0,0 +1,10 @@
+class Hellov{
+	public static void main(String args[])
+	{
+		System.out.println("Hello");
+		System.out.println("Name:Vishakha Rapelli");
+		System.out.println("Collage:A g patil");
+		System.out.println("I am from solapur");
+		
+	}
+}
