522005098a1330998f8ef53cf35d7ba3e15e02ba
VishakhaRapelli <125191036+VishakhaRapelli@users.noreply.github.com>
2023-03-16 14:36:10 +0530

    Add files via upload

---
+++ Addsubmuldiv.java
@@ -0,0 +1,19 @@
+class Addsubmuldiv{
+public static void main(String args[])
+{
+	System.out.println("The two number is:");
+	int a=10;
+	int b=5;
+	System.out.println(a+ " " +b);
+	int sum=a+b;
+	int sub=a-b;
+	int mul=a*b;
+	int div=a/b;
+	int rem=a%b;
+	System.out.println("The sum is:"+sum);
+	System.out.println("The sub is:"+sub);
+	System.out.println("The mul is:"+mul);
+	System.out.println("The div is:"+div);
+	System.out.println("The rem is:"+rem);
+}
+}
